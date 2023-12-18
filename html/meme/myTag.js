3a2da99267332564a654aed219f8b689a353d8f0
KYRIOTA <943422656@qq.com>
2023-12-18 06:05:44 -0800

    Site updated: 2023-12-18 06:05:43

---
+++ html/meme/myTag.js
@@ -0,0 +1,163 @@
+function getTag() {
+    return [
+
+        // '<div> \
+        //     <img src=\'imgs/test.jpg\' style=\"width: 100px; height: 100px;\"/> \
+        //     <center> \
+        //         test \
+        //     </center> \
+        // </div>',
+
+        // ['Hello World!', function () {
+        //     console.log(this);
+        //     $(this).css("color", randomColor);
+        //     setInterval(() => $(this).css("color", randomColor), 1000);
+        // }],
+
+        // '<a href="somepage" style="font-size: 4em">Click Me!</a>',
+        
+        // [
+        //     'Google',
+        //     function () { $(this).click(e => window.open('https://www.google.com')) }
+        // ]
+
+        '<div> \
+            <img src=\'imgs/kafu.jpg\'/> \
+            <center> \
+                <a href="https://ja.wikipedia.org/wiki/%E5%8F%AF%E4%B8%8D(KAFU)"> \
+                    KAFU/可不 \
+                </a> \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/cocoa.jpg\'/> \
+            <center> \
+                <a href="https://store.steampowered.com/app/400910/RabiRibi/"> \
+                    Rabi-Ribi \
+                </a> \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/tf2.png\'/> \
+            <center> \
+                <a href="https://store.steampowered.com/app/440/Team_Fortress_2/"> \
+                    TeamFortress2 \
+                </a> \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/portal.png\'/> \
+            <center> \
+                <a href="https://store.steampowered.com/app/620/Portal_2/"> \
+                    Portal \
+                </a> \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/PsychoPass.jpg\'/> \
+            <center> \
+                <a href="https://en.wikipedia.org/wiki/Psycho-Pass"> \
+                    Psycho-Pass \
+                </a> \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/hl2.jpg\'/> \
+            <center> \
+                <a href="https://store.steampowered.com/app/220/HalfLife_2/"> \
+                    Half-Life \
+                </a> \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/overlord.jpg\'/> \
+            <center> \
+                <a href="https://en.wikipedia.org/wiki/Overlord_(novel_series)"> \
+                    Overlord \
+                </a> \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/jojo.jpg\'/> \
+            <center> \
+                JOJO \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/luckyStar.jpg\'/> \
+            <center> \
+                <a href="https://en.wikipedia.org/wiki/Lucky_Star_(manga)"> \
+                    Lucky Star \
+                </a> \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/mlp.png\'/> \
+            <center> \
+                My Little Pony \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/tanya.jpg\'/> \
+            <center> \
+                <a href="https://ja.wikipedia.org/wiki/%E5%B9%BC%E5%A5%B3%E6%88%A6%E8%A8%98"> \
+                    Tanya / 幼女戦記 \
+                </a> \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/mahjong.jpg\'/> \
+            <center> \
+                <a href="https://en.wikipedia.org/wiki/Japanese_mahjong"> \
+                    Mahjong \
+                </a> \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/apex.webp\'/> \
+            <center> \
+                <a href="https://www.ea.com/games/apex-legends"> \
+                    Apex \
+                </a> \
+            </center> \
+        </div>',
+        
+        '<div> \
+            <img src=\'imgs/warframe.webp\'/> \
+            <center> \
+                <a href="https://www.warframe.com/"> \
+                    Warframe \
+                </a> \
+            </center> \
+        </div>',
+        
+        '<div> \
+            <img src=\'imgs/terraria.jpg\'/> \
+            <center> \
+                <a href="https://terraria.org/"> \
+                    Terraria \
+                </a> \
+            </center> \
+        </div>',
+
+        '<div> \
+            <img src=\'imgs/kaguya_luna.jpg\'/> \
+            <center> \
+                Kaguya Luna \
+            </center> \
+        </div>',
+
+    ];
+}
